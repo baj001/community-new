7adf095dc42ecc785cc07dc553d4f531fe66eb27
baj <fkshang@126.com>
2022-04-20 20:30:54 +0800

    网站数据统计

--- src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -1,7 +1,12 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 package com.nowcoder.community.config;
 
 import com.nowcoder.community.controller.interceptor.AlphaInterceptor;
-import com.nowcoder.community.controller.interceptor.LoginRequiredInterceptor;
+import com.nowcoder.community.controller.interceptor.DataInterceptor;
 import com.nowcoder.community.controller.interceptor.LoginTicketInterceptor;
 import com.nowcoder.community.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,33 +16,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
     @Autowired
     private AlphaInterceptor alphaInterceptor;
-
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
-
-//    @Autowired
-//    private LoginRequiredInterceptor loginRequiredInterceptor;
-
     @Autowired
     private MessageInterceptor messageInterceptor;
+    @Autowired
+    private DataInterceptor dataInterceptor;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(alphaInterceptor)
-                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg")
-                .addPathPatterns("/register", "/login");
-
-        registry.addInterceptor(loginTicketInterceptor)
-                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
-
-//        registry.addInterceptor(loginRequiredInterceptor)
-//                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
-
-        registry.addInterceptor(messageInterceptor)
-                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+    public WebMvcConfig() {
     }
 
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this.alphaInterceptor).excludePathPatterns(new String[]{"/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg"}).addPathPatterns(new String[]{"/register", "/login"});
+        registry.addInterceptor(this.loginTicketInterceptor).excludePathPatterns(new String[]{"/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg"});
+        registry.addInterceptor(this.messageInterceptor).excludePathPatterns(new String[]{"/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg"});
+        registry.addInterceptor(this.dataInterceptor).excludePathPatterns(new String[]{"/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg"});
+    }
 }
